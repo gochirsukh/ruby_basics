ab1a19bc51cf9de34d5896ea973e465f21ae69fa
Gansukh Ochirsukh <gansukh.ochirsukh@cerner.com>
2023-04-22 08:43:16 -0500

    hash

---
+++ hashes/hash_map_challange.rb
@@ -0,0 +1,12 @@
+number_hash = {
+    1 => 'I',
+    2 => 'II',
+    3 => 'III',
+    4 => 'IV',
+    5 => 'V'
+}
+
+# puts number_hash
+number_hash[4] = 'mee'
+# puts number_hash[4]
+puts number_hash
