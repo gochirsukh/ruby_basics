45fb4550645fc0ee11de01a0b8fd6346dc6579fe
Gansukh Ochirsukh <gansukh.ochirsukh@cerner.com>
2023-04-22 21:54:27 -0500

    else - Sat Apr 22 21:54:27 CDT 2023

--- control-structures/if-then-else-elsif/if_then_example.rb
+++ control-structures/if-then-else-elsif/if_then_example.rb
@@ -1,6 +1,9 @@
-fruit = 'orange'
-if fruit == 'apple'
-    puts fruit.upcase
-else 
-    puts 'something else'
+x = 20
+
+if x <= 10 
+    puts "10 or above"
+elsif x >= 20
+    puts "20 or more"
+else
+    puts "between 10 and 20"
 end
