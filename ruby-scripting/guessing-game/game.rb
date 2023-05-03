33b2f7a4e663c19ffd16e076be0d8a817f715aea
Gansukh Ochirsukh <gansukh.ochirsukh@cerner.com>
2023-05-03 08:38:02 -0500

    challange - Wed May  3 08:38:01 CDT 2023

---
+++ ruby-scripting/guessing-game/game.rb
@@ -0,0 +1,27 @@
+#!/bin/bash ruby
+
+
+#  TO DO
+# GO CHECKOUT OUT THE EXERSIZE FILE FOR THIS CHALLANGE
+
+# puts "-----------------"
+# puts "| Guessing Game |"
+# puts "-----------------"
+
+# random_number = rand(10)
+
+# puts "What's your name"
+# response = gets
+
+# puts "Nice to meet you #{response}"
+# puts "Enter a number"
+# guess = gets 
+
+# if guess == random_number
+#     puts " #{guess}  is the correct number"
+# elsif
+#     puts "wrong number, the correct number was #{random_number}"
+#     puts "try again"
+# end
+
+
